ea2d1643d5c5e42c25d967e996dfb406743eb8dc
Jack Lay <42285595+jackhlay@users.noreply.github.com>
2024-05-31 18:09:33 -0600

    Added readme
    Added a short summary of the project. Also added a dexterity stat for the player.

---
+++ README.md
@@ -0,0 +1,4 @@
+# Text adventure Dungeon Crawler  
+  
+## This is a text aventure game that uses procedural generaation to create the map and different floors.
+## It includes a secondary screen to show the map as the player progresses through the dungeon, and to view and interacti with the inventory.
--- src/DungeonGen/Player.java
+++ src/DungeonGen/Player.java
@@ -1,6 +1,7 @@
 public class Player {
     public int Level;
     public int Strength;
+    public int Dexterity;
     public int Gold;
     public int armorLevel;
 
@@ -9,6 +10,7 @@ public class Player {
     public Player(){
         this.Level = 0;
         this.Strength = 0;
+        this.Dexterity = 0;
         this.Gold=0;
         this.armorLevel=0;
         this.Inventory = new String[0];
